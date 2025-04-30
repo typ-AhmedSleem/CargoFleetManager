b641439028dc4d6330b8ae656d43436464f8cd1a
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2025-04-30 21:05:16 +0300

    feat: integrate GamepadController and update application entry point.
    - Removes the `Greeting` class.
    - Replaces `App()` with `CFMAppEntryPoint()` as the main entry point in `MainActivity`, `MainViewController`, and `main.kt` for wasmJs and desktop.
    - Adds `enableEdgeToEdge()` to `MainActivity` for edge-to-edge screen support.
    - Adds `GamepadController` integration in the desktop `main.kt`, including:
        - Displaying gamepad state information.
        - Implementing haptic feedback features.
        - Added `HttpClient` code, but it's commented out.
        - Added several buttons for the haptic feedback test.
    - Adds controller vibration functionality.
    - Add `anyControllerConnected` to check for any connected controllers.
    - Add `grabControllerState` function to get the state of a connected controller.
    - Update controller count display `controllersCount`.
    - Add `dispose()` function for the gamepad.

--- composeApp/src/desktopMain/kotlin/com/typ/cargo/main.kt
+++ composeApp/src/desktopMain/kotlin/com/typ/cargo/main.kt
@@ -2,7 +2,11 @@ package com.typ.cargo
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -14,44 +18,56 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.singleWindowApplication
-import com.badlogic.gdx.controllers.Controller
-import com.badlogic.gdx.controllers.Controllers
-import com.studiohartman.jamepad.ControllerManager
+import com.studiohartman.jamepad.ControllerPowerLevel
 import com.studiohartman.jamepad.ControllerState
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.takeFrom
 import kotlinx.coroutines.delay
 
 fun main() = singleWindowApplication(
     title = "CargoFleetManager",
 ) {
-    val gamepadController = remember { GamepadController() }
-    val manager = remember {
-        ControllerManager().apply {
-            initSDLGamepad()
-        }
-    }
+    val manager = remember { GamepadController() }
     var controllerState by remember {
         mutableStateOf<ControllerState?>(null)
+    }
+    var powerLevel by remember {
+        mutableStateOf<ControllerPowerLevel?>(null)
     }
 
     LaunchedEffect(Unit) {
         while (true) {
-            if (manager.numControllers > 0) {
-                controllerState = manager.getState(0)
+            if (manager.anyControllerConnected) {
+                controllerState = manager.grabControllerState()
                 delay(5)
+                if (controllerState?.aJustPressed == true) {
+                    manager.performHapticFeedback(
+                        leftMotor = 0.8f,
+                        rightMotor = 0.8f,
+                        duration = 5000
+                    )
+//                    powerLevel = manager.controller?.powerLevel
+                }
             }
-            manager.update()
         }
     }
 
     MaterialTheme {
         Column(
-            Modifier.fillMaxSize(),
+            Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             // * Display the controller state fields within Texts
-            Text("Connected Controllers: ${manager.numControllers}")
+            Text("Connected Controllers: ${manager.controllersCount}")
             Text("Button A: ${controllerState?.a}")
             Text("Button B: ${controllerState?.b}")
             Text("Button X: ${controllerState?.x}")
@@ -77,12 +93,93 @@ fun main() = singleWindowApplication(
             Text("Right Stick Angle: ${controllerState?.rightStickAngle}")
             Text("Left Stick Click: ${controllerState?.leftStickJustClicked}")
             Text("Right Stick Click: ${controllerState?.rightStickJustClicked}")
+            Text("Power level: $powerLevel")
+
+            val client = HttpClient {
+                defaultRequest {
+                    url {
+                        takeFrom("https://localhost.com") // Base url
+                    }
+                }
+                expectSuccess = true
+                install(HttpTimeout) {
+                    val timeout = 30000L
+                    connectTimeoutMillis = timeout
+                    requestTimeoutMillis = timeout
+                    socketTimeoutMillis = timeout
+                }
+                /*install(ContentNegotiation) {
+                    json(Json {
+                        ignoreUnknownKeys = true
+                    })
+                }*/
+            }
+
+            LaunchedEffect(Unit) {
+                client.get {
+                    url {
+                        takeFrom("https://localhost.com")
+                    }
+                }.bodyAsText()
+                    .also {
+                        println("Response: $it")
+                    }
+            }
+
+            Spacer(Modifier.height(8.dp))
+            Button(
+                onClick = {
+                    manager.performHapticFeedback(
+                        leftMotor = 0.5f,
+                        rightMotor = 0.5f,
+                        duration = 100
+                    )
+                }
+            ) {
+                Text("Perform full short haptic vibration")
+            }
+            Spacer(Modifier.height(8.dp))
+            Button(
+                onClick = {
+                    manager.performHapticFeedback(
+                        leftMotor = 0.5f,
+                        rightMotor = 0.5f,
+                        duration = 100
+                    )
+                }
+            ) {
+                Text("Perform full long haptic vibration")
+            }
+            Spacer(Modifier.height(8.dp))
+            Button(
+                onClick = {
+                    manager.performHapticFeedback(
+                        leftMotor = 1f,
+                        rightMotor = 0f,
+                        duration = 500
+                    )
+                }
+            ) {
+                Text("Perform left haptic vibration")
+            }
+            Spacer(Modifier.height(8.dp))
+            Button(
+                onClick = {
+                    manager.performHapticFeedback(
+                        leftMotor = 0f,
+                        rightMotor = 1f,
+                        duration = 500
+                    )
+                }
+            ) {
+                Text("Perform right haptic vibration")
+            }
         }
     }
 
     DisposableEffect(Unit) {
         onDispose {
-            manager.quitSDLGamepad()
+            manager.dispose()
         }
     }
 }
--- composeApp/src/iosMain/kotlin/com/typ/cargo/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/typ/cargo/MainViewController.kt
@@ -2,4 +2,4 @@ package com.typ.cargo
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { CFMAppEntryPoint() }
