e1585c79451ce02404f45340012a96e517a5b5ba
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2025-05-01 01:31:18 +0300

    feat: refactor DashboardScreen UI into Left, Center, and Right panels.
    The DashboardScreen UI has been restructured into three distinct panels: DashboardLeftPanel, DashboardCenterPanel, and DashboardRightPanel. This change improves organization and maintainability. Additionally, a ThreeEqualSectionsRow composable was introduced to manage the layout of these panels within the screen.

--- composeApp/src/commonMain/kotlin/com/typ/cargo/ui/screens/dashboard/DashboardScreen.kt
+++ composeApp/src/commonMain/kotlin/com/typ/cargo/ui/screens/dashboard/DashboardScreen.kt
@@ -1,28 +1,21 @@
 package com.typ.cargo.ui.screens.dashboard
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
-import com.typ.cargo.ui.converters.UiDrivingModeConverter
-import com.typ.cargo.ui.screens.dashboard.components.BatteryDetailsCard
-import com.typ.cargo.ui.screens.dashboard.components.DashboardCard
-import com.typ.cargo.ui.screens.dashboard.components.DriversTemperatureCard
-import com.typ.cargo.ui.screens.dashboard.components.MotorSpeedCard
-import com.typ.cargo.ui.screens.dashboard.components.RobotHeadingCard
-import com.typ.cargo.ui.theming.Colors
+import com.typ.cargo.ui.screens.dashboard.components.DashboardCenterPanel
+import com.typ.cargo.ui.screens.dashboard.components.DashboardLeftPanel
+import com.typ.cargo.ui.screens.dashboard.components.DashboardRightPanel
+import com.typ.cargo.ui.screens.dashboard.components.ThreeEqualSectionsRow
 import com.typ.cargo.viewmodels.DashboardViewModel
-import io.github.alexzhirkevich.cupertino.CupertinoText
 import io.github.alexzhirkevich.cupertino.theme.CupertinoTheme
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
@@ -30,64 +23,20 @@ import org.koin.core.component.get
 object DashboardScreen : Screen, KoinComponent {
     @Composable
     override fun Content() {
+        // * Runtime * //
         val viewModel: DashboardViewModel = remember { get() }
-        val data by viewModel.dashboardData
+        val robotInfo by viewModel.dashboardData
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            CupertinoText(
-                text = "Cargo Fleet Manager",
-                fontSize = 50.sp,
-                style = CupertinoTheme.typography.largeTitle,
-                modifier = Modifier.padding(
-                    vertical = 24.dp,
-                    horizontal = 8.dp
-                ),
-            )
-
-            data?.let {
-                BatteryDetailsCard(carBatteryPercentage = it.batteryPercentage)
-
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally)
-                ) {
-                    MotorSpeedCard(
-                        title = "Left Motor Speed",
-                        modifier = Modifier.weight(1f),
-                        motorSpeed = it.leftMotorSpeed
-                    )
-                    MotorSpeedCard(
-                        title = "Right Motor Speed",
-                        modifier = Modifier.weight(1f),
-                        motorSpeed = it.rightMotorSpeed
-                    )
-                }
-
-                DriversTemperatureCard(temperature = it.driversTemperature)
-
-                DashboardCard(
-                    title = "Driving Mode",
-                    valueTextColor = CupertinoTheme.colorScheme.accent,
-                    value = UiDrivingModeConverter.convert(it.drivingMode)
-                )
-
-                RobotHeadingCard(robotHeading = it.robotHeading)
-
-                DashboardCard(
-                    title = "Errors",
-                    valueTextColor = when {
-                        it.errorLogs.isNullOrEmpty() -> CupertinoTheme.colorScheme.label
-                        else -> Colors.error
-                    },
-                    value = it.errorLogs ?: "No errors have been reported yet"
-                )
-            }
+        // * UI * //
+        ThreeEqualSectionsRow(Modifier
+            .fillMaxSize()
+            .padding(
+                vertical = 16.dp,
+                horizontal = 16.dp
+            )) {
+            DashboardLeftPanel(robotInfo, 0.65f)
+            DashboardCenterPanel(robotInfo, 1f)
+            DashboardRightPanel(0.65f)
         }
     }
 }
