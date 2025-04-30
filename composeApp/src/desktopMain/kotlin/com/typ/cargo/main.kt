3121772cacd9cc5e86073c786e2fcd96df7430a8
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2025-04-30 23:31:22 +0300

    feat: configure default window state in desktop app.

--- composeApp/src/desktopMain/kotlin/com/typ/cargo/main.kt
+++ composeApp/src/desktopMain/kotlin/com/typ/cargo/main.kt
@@ -1,9 +1,21 @@
 package com.typ.cargo
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowPosition.Aligned
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.singleWindowApplication
 
 fun main() = singleWindowApplication(
     title = "CargoFleetManager",
+    state = WindowState(
+        position = Aligned(Alignment.Center),
+        placement = WindowPlacement.Maximized,
+        width = 1280.dp,
+        height = 720.dp
+    )
 ) {
     CFMAppEntryPoint()
 }
