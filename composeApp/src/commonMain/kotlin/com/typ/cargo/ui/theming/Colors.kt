edf37a66e21ec1f95619696ebeb110e29145bb0e
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2025-04-30 23:23:48 +0300

    feat: add Colors object with success, warning, and error colors.

---
+++ composeApp/src/commonMain/kotlin/com/typ/cargo/ui/theming/Colors.kt
@@ -0,0 +1,9 @@
+package com.typ.cargo.ui.theming
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+    val success: Color = Color(0xFF4CAF50)
+    val warning: Color = Color(0xFFFFC107)
+    val error: Color = Color(0xFFF44336)
+}
