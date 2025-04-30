5e830798b77952e8f2a6ee845fc3f58c52a54523
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2025-04-30 21:05:29 +0300

    feat: replace LibGDX with Jamepad for gamepad support.

--- composeApp/src/desktopMain/kotlin/com/typ/cargo/GamePadManager.kt
+++ composeApp/src/desktopMain/kotlin/com/typ/cargo/GamePadManager.kt
@@ -2,9 +2,57 @@ package com.typ.cargo
 
 import com.badlogic.gdx.controllers.Controller
 import com.badlogic.gdx.controllers.ControllerListener
-import com.badlogic.gdx.controllers.Controllers
+import com.studiohartman.jamepad.ControllerIndex
+import com.studiohartman.jamepad.ControllerManager
+import com.studiohartman.jamepad.ControllerState
+import kotlinx.coroutines.delay
 
 class GamepadController : ControllerListener {
+
+    private var controller: ControllerIndex? = null
+        private set(value) {
+            if (value != null) {
+                println("[GamepadController]: Controller has been connected successfully.")
+            }
+            field = value
+        }
+    private val controllerManager = ControllerManager()
+        .apply {
+            initSDLGamepad()
+        }
+        .also {
+            if (it.numControllers >= 0) {
+                controller = it.getControllerIndex(0)
+            }
+        }
+
+    val controllersCount: Int
+        get() = controllerManager.numControllers
+
+    val anyControllerConnected: Boolean
+        get() = controllersCount >= 0
+
+    suspend fun grabControllerState(controllerIndex: Int = 0): ControllerState {
+        val state = controllerManager.getState(controllerIndex)
+        delay(5L)
+        controllerManager.update()
+        return state
+    }
+
+    fun performHapticFeedback(
+        controllerIdx: Int = 0,
+        leftMotor: Float,
+        rightMotor: Float,
+        duration: Int = 100
+    ): Boolean {
+        return controller?.doVibration(
+            leftMotor,
+            rightMotor,
+            duration
+        ).also { vibrated ->
+            println("performHapticFeedback: canVibrate= ${controller?.canVibrate()}, vibrated= $vibrated")
+        } == true
+    }
 
     override fun connected(controller: Controller) {
         println("🎮 Gamepad Connected: ${controller.name}")
@@ -28,4 +76,13 @@ class GamepadController : ControllerListener {
         println("🕹️ Joystick Moved - Axis: $axisIndex, Value: $value")
         return false
     }
+
+    fun dispose() {
+        try {
+            controllerManager.quitSDLGamepad()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+    }
+
 }
