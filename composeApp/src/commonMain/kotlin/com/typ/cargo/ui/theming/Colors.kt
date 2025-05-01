a937c14c7bb785888908f660f655b3e1e05af004
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2025-05-01 12:35:57 +0300

    ref: added more fields in Colors.

--- composeApp/src/commonMain/kotlin/com/typ/cargo/ui/theming/Colors.kt
+++ composeApp/src/commonMain/kotlin/com/typ/cargo/ui/theming/Colors.kt
@@ -1,9 +1,22 @@
 package com.typ.cargo.ui.theming
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import io.github.alexzhirkevich.cupertino.theme.CupertinoTheme
 
 object Colors {
     val success: Color = Color(0xFF4CAF50)
     val warning: Color = Color(0xFFFFC107)
     val error: Color = Color(0xFFF44336)
+
+    // * Theme related colors * //
+    val label: Color
+        @Composable
+        get() = CupertinoTheme.colorScheme.label
+    val accent: Color
+        @Composable
+        get() = CupertinoTheme.colorScheme.accent
+    val secondarySystemBackground: Color
+        @Composable
+        get() = CupertinoTheme.colorScheme.secondarySystemBackground
 }
