e7fa8676f97ee52068bc70a140892e694a434d26
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2025-05-02 21:16:13 +0300

    refactor: rename videoFeed endpoint to videoStream.

--- composeApp/src/commonMain/kotlin/com/typ/cargo/networking/NetworkConstants.kt
+++ composeApp/src/commonMain/kotlin/com/typ/cargo/networking/NetworkConstants.kt
@@ -9,7 +9,7 @@ const val AMR_DEFAULT_IP = "0.0.0.0"
 const val AMR_BASE_URL = "http://$AMR_DEFAULT_IP:$AMR_DEFAULT_PORT"
 
 object Endpoints {
-    val videoFeed = buildEndpoint("videoFeed")
+    val videoStream = buildEndpoint("videoFeed")
 }
 
 fun buildEndpoint(endpoint: String) = "$AMR_BASE_URL/$endpoint"
