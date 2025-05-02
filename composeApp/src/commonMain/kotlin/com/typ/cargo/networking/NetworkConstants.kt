9b85dbb1a86090f91a597d82358e6dcfc831b985
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2025-05-02 17:54:44 +0300

    feat: add network constants and endpoints.

---
+++ composeApp/src/commonMain/kotlin/com/typ/cargo/networking/NetworkConstants.kt
@@ -0,0 +1,14 @@
+package com.typ.cargo.networking
+
+const val AMR_DEFAULT_IP = "0.0.0.0"
+const val AMR_DEFAULT_PORT = 2001
+const val AMR_DEFAULT_TIMEOUT = 30000L
+const val AMR_DEFAULT_MAX_RETRIES = 15
+const val AMR_DEFAULT_RETRY_DELAY = 1000L
+const val AMR_BASE_URL = "http://$AMR_DEFAULT_IP:$AMR_DEFAULT_PORT"
+
+object Endpoints {
+    val videoFeed = buildEndpoint("videoFeed")
+}
+
+fun buildEndpoint(endpoint: String) = "$AMR_BASE_URL/$endpoint"
