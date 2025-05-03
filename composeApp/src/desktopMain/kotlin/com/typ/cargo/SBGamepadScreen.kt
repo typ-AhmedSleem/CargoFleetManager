3fc5034b3bceeae62449a9b3b6260a452c026eff
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2025-05-03 18:48:59 +0300

    feat: add gamepad controller state display and haptic feedback.
    This commit introduces the ability to display the state of a connected gamepad controller, including button presses, stick movements, and power level. It also adds functionality for performing haptic feedback on the controller, with options for full, left, and right motor vibrations. Additionally, the commit refactors the code to utilize a coroutine-based subscription for controller updates, providing real-time state changes and connection status. A button has also been added to scan for controllers.

--- composeApp/src/desktopMain/kotlin/com/typ/cargo/SBGamepadScreen.kt
+++ composeApp/src/desktopMain/kotlin/com/typ/cargo/SBGamepadScreen.kt
@@ -16,174 +16,200 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import com.studiohartman.jamepad.ControllerPowerLevel
-import com.studiohartman.jamepad.ControllerState
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
-import io.ktor.http.takeFrom
+import com.typ.cargo.features.gamepad.ControllerSnapshot
+import com.typ.cargo.features.gamepad.GamepadController
+import io.github.alexzhirkevich.cupertino.CupertinoButton
+import io.github.alexzhirkevich.cupertino.CupertinoText
+import io.github.alexzhirkevich.cupertino.ExperimentalCupertinoApi
+import io.github.alexzhirkevich.cupertino.theme.CupertinoTheme
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
-object SBGamepadScreen: Screen {
+object SBGamepadScreen : Screen {
 
     private fun readResolve(): Any = SBGamepadScreen
 
+    @OptIn(ExperimentalCupertinoApi::class)
     @Composable
     override fun Content() {
-        val manager = remember { GamepadController() }
-        var controllerState by remember {
-            mutableStateOf<ControllerState?>(null)
-        }
+        val coroutineScope = rememberCoroutineScope()
+        val controller = remember { GamepadController() }
         var powerLevel by remember {
-            mutableStateOf<ControllerPowerLevel?>(null)
+            mutableStateOf(ControllerPowerLevel.POWER_UNKNOWN)
+        }
+        var controllerState by remember {
+            mutableStateOf(controller.captureState())
+        }
+        var connected by remember {
+            mutableStateOf(false)
         }
 
-        LaunchedEffect(Unit) {
-            while (true) {
-                if (manager.anyControllerConnected) {
-                    controllerState = manager.grabControllerState()
-                    delay(5)
-                    if (controllerState?.aJustPressed == true) {
-                        manager.performHapticFeedback(
-                            leftMotor = 0.8f,
-                            rightMotor = 0.8f,
-                            duration = 5000
-                        )
-//                    powerLevel = manager.controller?.powerLevel
+        LaunchedEffect(connected) {
+            if (connected) {
+                println("Controller is connected and start to subscribe.")
+                do {
+                    connected = controllerState.isConnected
+                    controllerState = controller.captureState()
+                    powerLevel = controller.powerLevel
+                } while (connected)
+
+                controller
+                    .subscribe()
+                    .flowOn(kotlinx.coroutines.Dispatchers.Default)
+                    .onStart {
+                        println("Controller subscription has started.")
                     }
-                }
+                    .onCompletion {
+                        connected = false
+                        println("Controller subscription has completed.")
+                    }
+                    .collect { snapshot ->
+                        when (snapshot) {
+                            is ControllerSnapshot.Snapshot -> {
+                                connected = snapshot.controllerState.isConnected
+                                controllerState = snapshot.controllerState
+                                powerLevel = snapshot.powerLevel
+                            }
+
+                            is ControllerSnapshot.Disconnected -> {
+                                connected = false
+                                println("Controller has been disconnected.")
+                            }
+                        }
+                    }.also {
+                        println("Controller subscription has been launched.")
+                    }
             }
         }
 
         MaterialTheme {
+/*            AnimatedContent(
+                targetState = connected,
+                modifier = Modifier.fillMaxSize(),
+            ) { isConnected ->*/
             Column(
                 Modifier
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState()),
-                verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(
+                    alignment = Alignment.CenterVertically,
+                    space = 4.dp,
+                ),
             ) {
-                // * Display the controller state fields within Texts
-                Text("Connected Controllers: ${manager.controllersCount}")
-                Text("Button A: ${controllerState?.a}")
-                Text("Button B: ${controllerState?.b}")
-                Text("Button X: ${controllerState?.x}")
-                Text("Button Y: ${controllerState?.y}")
-                Text("Button L1: ${controllerState?.lb}")
-                Text("Button R1: ${controllerState?.rb}")
-                Text("Button L2: ${controllerState?.leftTrigger}")
-                Text("Button R2: ${controllerState?.rightTrigger}")
-                Text("Button Back: ${controllerState?.back}")
-                Text("Button Start: ${controllerState?.start}")
-                Text("Button Guide: ${controllerState?.guide}")
-                Text("DPad Up: ${controllerState?.dpadUpJustPressed}")
-                Text("DPad Down: ${controllerState?.dpadDownJustPressed}")
-                Text("DPad Left: ${controllerState?.dpadLeftJustPressed}")
-                Text("DPad Right: ${controllerState?.dpadRightJustPressed}")
-                Text("Left Stick X: ${controllerState?.leftStickX}")
-                Text("Left Stick Y: ${controllerState?.leftStickY}")
-                Text("Right Stick X: ${controllerState?.rightStickX}")
-                Text("Right Stick Y: ${controllerState?.rightStickY}")
-                Text("Left Stick Magnitude: ${controllerState?.leftStickMagnitude}")
-                Text("Right Stick Magnitude: ${controllerState?.rightStickMagnitude}")
-                Text("Left Stick Angle: ${controllerState?.leftStickAngle}")
-                Text("Right Stick Angle: ${controllerState?.rightStickAngle}")
-                Text("Left Stick Click: ${controllerState?.leftStickJustClicked}")
-                Text("Right Stick Click: ${controllerState?.rightStickJustClicked}")
-                Text("Power level: $powerLevel")
+                if (connected) {
+                    // * Display the controller state fields within Texts
+                    Text("Power level: $powerLevel")
+                    Text("Button A: ${controllerState.a}")
+                    Text("Button B: ${controllerState.b}")
+                    Text("Button X: ${controllerState.x}")
+                    Text("Button Y: ${controllerState.y}")
+                    Text("Button L1: ${controllerState.lb}")
+                    Text("Button R1: ${controllerState.rb}")
+                    Text("Button L2: ${controllerState.leftTrigger}")
+                    Text("Button R2: ${controllerState.rightTrigger}")
+                    Text("Button Back: ${controllerState.back}")
+                    Text("Button Start: ${controllerState.start}")
+                    Text("Button Guide: ${controllerState.guide}")
+                    Text("DPad Up: ${controllerState.dpadUpJustPressed}")
+                    Text("DPad Down: ${controllerState.dpadDownJustPressed}")
+                    Text("DPad Left: ${controllerState.dpadLeftJustPressed}")
+                    Text("DPad Right: ${controllerState.dpadRightJustPressed}")
+                    Text("Left Stick X: ${controllerState.leftStickX}")
+                    Text("Left Stick Y: ${controllerState.leftStickY}")
+                    Text("Right Stick X: ${controllerState.rightStickX}")
+                    Text("Right Stick Y: ${controllerState.rightStickY}")
+                    Text("Left Stick Magnitude: ${controllerState.leftStickMagnitude}")
+                    Text("Right Stick Magnitude: ${controllerState.rightStickMagnitude}")
+                    Text("Left Stick Angle: ${controllerState.leftStickAngle}")
+                    Text("Right Stick Angle: ${controllerState.rightStickAngle}")
+                    Text("Left Stick Click: ${controllerState.leftStickJustClicked}")
+                    Text("Right Stick Click: ${controllerState.rightStickJustClicked}")
 
-                val client = HttpClient {
-                    defaultRequest {
-                        url {
-                            takeFrom("https://localhost.com") // Base url
+                    Spacer(Modifier.height(8.dp))
+                    Button(
+                        onClick = {
+                            controller.performHapticFeedback(
+                                leftMotor = 0.5f,
+                                rightMotor = 0.5f,
+                                duration = 100
+                            )
                         }
+                    ) {
+                        Text("Perform full short haptic vibration")
                     }
-                    expectSuccess = true
-                    install(HttpTimeout) {
-                        val timeout = 30000L
-                        connectTimeoutMillis = timeout
-                        requestTimeoutMillis = timeout
-                        socketTimeoutMillis = timeout
-                    }
-                    /*install(ContentNegotiation) {
-                        json(Json {
-                            ignoreUnknownKeys = true
-                        })
-                    }*/
-                }
-
-                LaunchedEffect(Unit) {
-                    client.get {
-                        url {
-                            takeFrom("https://localhost.com")
+                    Spacer(Modifier.height(8.dp))
+                    Button(
+                        onClick = {
+                            controller.performHapticFeedback(
+                                leftMotor = 0.5f,
+                                rightMotor = 0.5f,
+                                duration = 100
+                            )
                         }
-                    }.bodyAsText()
-                        .also {
-                            println("Response: $it")
+                    ) {
+                        Text("Perform full long haptic vibration")
+                    }
+                    Spacer(Modifier.height(8.dp))
+                    Button(
+                        onClick = {
+                            controller.performHapticFeedback(
+                                leftMotor = 1f,
+                                rightMotor = 0f,
+                                duration = 500
+                            )
                         }
-                }
-
-                Spacer(Modifier.height(8.dp))
-                Button(
-                    onClick = {
-                        manager.performHapticFeedback(
-                            leftMotor = 0.5f,
-                            rightMotor = 0.5f,
-                            duration = 100
+                    ) {
+                        Text("Perform left haptic vibration")
+                    }
+                    Spacer(Modifier.height(8.dp))
+                    Button(
+                        onClick = {
+                            controller.performHapticFeedback(
+                                leftMotor = 0f,
+                                rightMotor = 1f,
+                                duration = 500
+                            )
+                        }
+                    ) {
+                        Text("Perform right haptic vibration")
+                    }
+                } else {
+                    // * Not connected * //
+                    Text("Not connected")
+                    Spacer(Modifier.height(8.dp))
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+                                val foundController = controller.searchForControllers()
+                                println("Found a controller: $foundController")
+                                connected = foundController
+                            }
+                        }
+                    ) {
+                        Text(
+                            if (connected) "Controller found"
+                            else "Search for controllers"
                         )
                     }
-                ) {
-                    Text("Perform full short haptic vibration")
-                }
-                Spacer(Modifier.height(8.dp))
-                Button(
-                    onClick = {
-                        manager.performHapticFeedback(
-                            leftMotor = 0.5f,
-                            rightMotor = 0.5f,
-                            duration = 100
-                        )
-                    }
-                ) {
-                    Text("Perform full long haptic vibration")
-                }
-                Spacer(Modifier.height(8.dp))
-                Button(
-                    onClick = {
-                        manager.performHapticFeedback(
-                            leftMotor = 1f,
-                            rightMotor = 0f,
-                            duration = 500
-                        )
-                    }
-                ) {
-                    Text("Perform left haptic vibration")
-                }
-                Spacer(Modifier.height(8.dp))
-                Button(
-                    onClick = {
-                        manager.performHapticFeedback(
-                            leftMotor = 0f,
-                            rightMotor = 1f,
-                            duration = 500
-                        )
-                    }
-                ) {
-                    Text("Perform right haptic vibration")
                 }
             }
+//            }
         }
 
         DisposableEffect(Unit) {
             onDispose {
-                manager.dispose()
+                controller.dispose()
             }
         }
     }
